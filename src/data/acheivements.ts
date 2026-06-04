faec71880ab3889e2e74f7d2be77fe3478945e1c
Jame <Jamesferris447@gmail.com>
2026-06-04 20:15:14 +1000

    set up achievements (mostly)

---
+++ src/data/acheivements.ts
@@ -0,0 +1,6 @@
+export const ACHIEVEMENTS = {
+    FIRST_VISIT: { id: 'FIRST_VISIT', title: 'Too Far in Now', desc: 'Your first mistake was visiting this site...'
+    },
+    VIEW_PROJECTS: { id: 'VIEW_PROJECTS', title: 'Secret Admirer', desc: 'Awww, I have a new biggest fan <3'
+    },
+}
