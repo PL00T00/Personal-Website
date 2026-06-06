32816ae9da6283a8ee0a186186fdf25868908f5e
Jame <Jamesferris447@gmail.com>
2026-06-06 16:29:28 +1000

    Actually fixed achievements

---
+++ src/data/achievements.js
@@ -0,0 +1,7 @@
+export const ACHIEVEMENTS = {
+    FIRST_VISIT: {
+        id: "FIRST_VISIT",
+        title: "👋 Too Far in Now",
+        desc: "Your first mistake was visiting this site..."
+    }
+};
