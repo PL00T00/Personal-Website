dd0481d9c6672957332783b4b8b53024c400b1be
Jame <Jamesferris447@gmail.com>
2026-06-08 15:28:58 +1000

    added achievements

--- src/data/achievements.js
+++ src/data/achievements.js
@@ -5,9 +5,21 @@ export const ACHIEVEMENTS = {
         desc: "Your first mistake was visiting this site..."
     },
 
-    BUTTON_PRESS: {
-        id: "BUTTON_PRESS",
-        title: "👇 Big Red Button",
-        desc: "Did you ask for consent before you touched that button?"
-    }
+    VIEW_PROJECTS: {
+        id: "VIEW_PROJECTS",
+        title: "💜 New Biggest Fan",
+        desc: "I can't belive I have a new biggest fan!"
+    },
+
+    TYPE_PIGEON: {
+        id: "TYPE_PIGEON",
+        title: "🦤 Birb",
+        desc: "Birb"
+    },
+
+    REVIEWER: {
+        id: "REVIEWER",
+        title: "🫶 Thankyou for your Service",
+        desc: "You should like tottally acccept this project"
+    },
 };
